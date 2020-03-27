1fcdb8b0dca0105b6e2970ee784fae2957f8616f
Hugo <h@hugo.sx>
2020-03-27 23:18:00 +1100

    Pass in directory name to express.static

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ const instance = axios.create({
   const app = express();
   const port = 8080;
 
-  app.use('/', express.static('public'));
+  app.use('/', express.static(__dirname + '/public'));
 
   app.get('/invoice', async (req, res) => {
     const {

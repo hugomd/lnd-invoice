9c2ab81ee757e9b0895377cf9a57c09c56476774
Hugo <h@hugo.sx>
2020-03-27 23:00:27 +1100

    Add express server to make calls to LND and generate QR code image

---
+++ src/index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const axios = require('axios');
+const https = require('https');
+const qrcode = require('qrcode');
+
+const {LND_IP, LND_PORT, MACAROON_BASE64} = process.env;
+
+if (!LND_IP || !LND_PORT || !MACAROON_BASE64) {
+  console.log(
+    'LND_IP, LND_PORT, MACAROON_BASE64 are required environment variables',
+  );
+  process.exit(1);
+}
+
+const instance = axios.create({
+  httpsAgent: new https.Agent({
+    rejectUnauthorized: false,
+  }),
+});
+
+(async () => {
+  const app = express();
+  const port = 8080;
+
+  app.use('/', express.static('public'));
+
+  app.get('/invoice', async (req, res) => {
+    const {
+      query: {memo = '', value},
+    } = req;
+
+    if (!value) {
+      res.send({error: 'value (sats) required'});
+    }
+
+    const {data} = await instance({
+      method: 'POST',
+      url: `https://${LND_IP}:${LND_PORT}/v1/invoices`,
+      headers: {
+        'Grpc-Metadata-macaroon': MACAROON_BASE64,
+      },
+      data: {
+        memo,
+        value,
+      },
+    });
+
+    const qr = await qrcode.toDataURL(data.payment_request);
+
+    res.send({qr});
+  });
+
+  app.listen(port, () => console.log(`Listening on port ${port}!`));
+})();
